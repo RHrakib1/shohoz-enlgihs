3a58098eea5946622cfe442935a0fb91ce349442
rakib hasan <rakibhasanmd457@gmail.com>
2026-03-03 15:26:52 +0600

    conditional error fixt

--- index.js
+++ index.js
@@ -16,15 +16,26 @@ const displaylevelone = (words) => {
     const wordcontianer = document.getElementById('word-contianer')
     wordcontianer.innerHTML = '';
 
+    if (words.length == 0) {
+        wordcontianer.innerHTML = `
+        <div class="text-center font-bangla col-span-full py-10 rounded  space-y-6">
+         <img class='mx-auto' src="/assets/alert-error.png" alt="">
+            <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="text-3xl font-bold">নেক্সট Lesson এ যান।</h2>
+        </div>`
+        return;
+    }
+
     for (let word of words) {
         console.log(word);
+
 
         const card = document.createElement('div')
         card.innerHTML = `
     <div class="bg-white shadow-sm rounded-xl text-center py-10 px-5 space-y-4">
-            <h2 class="text-2xl font-bold">${word.word}</h2>
+            <h2 class="text-2xl font-bold">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
             <p class="font-semibold">Meaning /Pronounciation</p>
-            <div class="font-medium text-xl font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+            <div class="font-medium text-xl font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায়নি"}"</div>
             <div class="flex justify-between items-center">
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF60]"><i
                         class="fa-solid fa-circle-info text-[#374957]"></i></button>
@@ -45,7 +56,6 @@ const displaylessons = (lessons) => {
 
     // 2. get into every leessons
     for (let lesson of lessons) {
-
         // create element
         const btndiv = document.createElement("div");
         btndiv.innerHTML = `
